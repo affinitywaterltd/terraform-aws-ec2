b391203dd59308ceeeeb7c2d5ce16a118eab244f
Haddrell <Glenn.Haddrell@affinitywater.co.uk>
2019-03-25 14:57:55 +0000

    Re-add ignore

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "default" {
   tags = "${var.tags}"
 
   lifecycle {
-    ignore_changes = ["ami", "user_data"]
+    ignore_changes = ["ami", "user_data", "root_block_device"]
   }
 }
 
